e6153285d79c460787e75b174ea272c5ca9e4421
Pratish Bhongle <bhonglepratish@gmail.com>
2025-06-16 10:08:39 +0530

    resolved build issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-// import NetflixSeries from "./components/NetfixSeries";//default export
+
 import  NetflixSeries from "./components/NetfixSeries";//Names export
 
-// import { Header } from "./components/NetfixSeries";
 
 export const App=()=>{
   return(
   <>
-
     <NetflixSeries/>
-
-
-
   </>
   )
 };
